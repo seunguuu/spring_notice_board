a90484dae1c7f5176a60f95717e5545254dac925
Seung Hun <youu0208@naver.com>
2024-03-25 08:50:56 +0900

    ADD BoardDao, BoardDaoImpl code

--- board-spring/src/main/java/com/hello/forum/bbs/dao/BoardDao.java
+++ board-spring/src/main/java/com/hello/forum/bbs/dao/BoardDao.java
@@ -1,6 +1,20 @@
 package com.hello.forum.bbs.dao;
 
+import java.util.List;
+
+import com.hello.forum.bbs.vo.BoardVO;
+
 public interface BoardDao {
 	
+	/**
+	 * DB에 저장된 모든 게시글의 수를 조회 
+	 * @return
+	 */
+	public int getBoardAllCount();
 	
+	/**
+	 * DB에 저장된 모든 게시글 목록을 조회 
+	 * @return
+	 */
+	public List<BoardVO> getAllBoard();
 }
---
+++ board-spring/src/main/java/com/hello/forum/bbs/dao/BoardDaoImpl.java
@@ -0,0 +1,19 @@
+package com.hello.forum.bbs.dao;
+
+import java.util.List;
+
+import com.hello.forum.bbs.vo.BoardVO;
+
+public class BoardDaoImpl implements BoardDao {
+
+	@Override
+	public int getBoardAllCount() {
+		return 0;
+	}
+
+	@Override
+	public List<BoardVO> getAllBoard() {
+		return null;
+	}
+
+}
