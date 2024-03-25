f424e9be3f1d22077e0889be1894d93be1335403
Seung Hun <youu0208@naver.com>
2024-03-25 18:13:20 +0900

    Add BoardDaoImpl, BoardDaoMapper code

--- board-spring/src/main/java/com/hello/forum/bbs/dao/BoardDaoImpl.java
+++ board-spring/src/main/java/com/hello/forum/bbs/dao/BoardDaoImpl.java
@@ -2,13 +2,28 @@ package com.hello.forum.bbs.dao;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.hello.forum.bbs.vo.BoardVO;
 
-public class BoardDaoImpl implements BoardDao {
+@Repository
+public class BoardDaoImpl extends SqlSessionDaoSupport implements BoardDao {
 
+	@Autowired
+	@Override
+	public void setSqlSessionTemplate(SqlSessionTemplate sqlSessionTemplate) {
+		super.setSqlSessionTemplate(sqlSessionTemplate);
+	}
+	
 	@Override
 	public int getBoardAllCount() {
-		return 0;
+		// BoardDaoMapper.xml에 있는 namespace에서 동작하고 싶은 쿼리의 id를 적어줘야 한다.
+		// 그 결과를 return
+		// 하나의 목록을 조회할 것이기 때문에 selectOne을 사용해준다.
+		return getSqlSession().selectOne("com.hello.forum.bbs.dao.BoardDao.getBoardAllCount");
 	}
 
 	@Override
