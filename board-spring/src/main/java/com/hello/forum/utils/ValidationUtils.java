78c58ef54ce23c7ebf1eba3444155bd7eaaf5195
Seung Hun <youu0208@naver.com>
2024-04-10 16:32:11 +0900

    Validation 기능 추가 (size, isEquals)

--- board-spring/src/main/java/com/hello/forum/utils/ValidationUtils.java
+++ board-spring/src/main/java/com/hello/forum/utils/ValidationUtils.java
@@ -1,17 +1,29 @@
 package com.hello.forum.utils;
 
 public class ValidationUtils {
-	
+
 	private ValidationUtils() {
 		
 	}
-
+	
 	public final static boolean notEmpty(String value) {
 		return ! StringUtils.isEmpty(value);
 	}
 	
 	public final static boolean email(String value) {
 		return StringUtils.isEmailFormat(value);
+	}
+	
+	public final static boolean size(String value, int minSize) {
+		return StringUtils.isEnough(value, minSize);
+	}
+	
+	public final static boolean isEquals(String value, String otherValue) {
+		if (StringUtils.isEmpty(value) || StringUtils.isEmpty(otherValue)) {
+			return false;
+		}
+		
+		return value.equals(otherValue);
 	}
 	
 }
